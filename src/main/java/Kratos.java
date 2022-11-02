43b7e88f020fe916362e42a68387d968f23c2eab
Bushmakin Mikhail <mbushmakin@vsegda-da.com>
2022-11-02 19:13:10 +0400

    Task* #3 GetterAndSetter

---
+++ src/main/java/Kratos.java
@@ -0,0 +1,14 @@
+public class Kratos {
+
+    private int speedFlightLeviathan = 152;
+
+    public int getSpeedFlightLeviathan() {
+        return speedFlightLeviathan;
+    }
+
+    public void setSpeedFlightLeviathan(int speedFlightLeviathan) {
+        this.speedFlightLeviathan = speedFlightLeviathan;
+    }
+
+
+}
